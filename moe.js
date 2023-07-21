3a0181771a72c762554346d2ea54e5c5d779e9a7
crisfrndz <114977127+crisfrndz@users.noreply.github.com>
2023-07-21 15:57:13 +0800

    Add files via upload

---
+++ moe.js
@@ -0,0 +1,34 @@
+function compMoe() {
+    let z = parseFloat(document.getElementById("z").value);
+    let p = parseFloat(document.getElementById("p").value);
+    let s = parseFloat(document.getElementById("s").value);
+    let n = parseFloat(document.getElementById("n").value);
+
+    if (p == 0) {
+        let moe = z * s / Math.sqrt(n);
+        let moepercent = moe * 100;
+        alert("Margin of error: " + moe.toFixed(3) + "\nPercentage: " + moepercent.toFixed(2) + "%");
+        document.getElementById("xmoe1").innerHTML = "MOE: " + moe.toFixed(3);
+        document.getElementById("perc1").innerHTML = "Percentage: " + moepercent.toFixed(2) + "%"; 
+        document.getElementById("solution1").style.display = "block";
+        document.getElementById("solution2").style.display = "none";
+        document.getElementById("zz1").innerHTML = z;
+        document.getElementById("ss1").innerHTML = p;
+        document.getElementById("nn1").innerHTML = n;
+
+    } else if (p > 0) {
+        let moe = z * Math.sqrt((p * (1 - p)) / n);
+        let moepercent = moe * 100;
+        alert("Margin of error: " + moe.toFixed(3) + "\nPercentage: " + moepercent.toFixed(2) + "%");
+        document.getElementById("solution2").style.display = "block";
+        document.getElementById("solution1").style.display = "none";
+        document.getElementById("xmoe2").innerHTML = "MOE: " + moe.toFixed(3);
+        document.getElementById("perc2").innerHTML = "Percentage: " + moepercent.toFixed(2) + "%"; 
+        document.getElementById("zz2").innerHTML = z;
+        document.getElementById("pp2").innerHTML = p;
+        document.getElementById("nn2").innerHTML = n;
+
+    } else {
+        alert("Invalid, please try again");
+    }
+}
